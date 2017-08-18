85c183de872ff693de14d664c82924aa0faa2237
Marin <Marin.hysollari@gmail.com>
2017-08-18 09:39:06 +0200

    admin part

--- resources/views/client/pages/profile.blade.php
+++ resources/views/client/pages/profile.blade.php
@@ -2,40 +2,55 @@
 @section('content')
     <div class="row">
         <div class="container-fluid">
-            <h4>Registration</h4>
+            <h4>Your Profile</h4>
             <div class="row" style="margin-bottom: 20px;">
                 <form id="profile-form" method="post" style="width: 100%" action="{{route('register.autobot')}}" enctype="multipart/form-data">
                     <div class="form-elem">
                         <div class="default-inp form-elem">
-                            <label for="full_name">Full Name</label>
-                            <input type="text" name="full_name" id="full_name"
-                                   placeholder="Full Name">
+                            <label for="telephone">Telephone</label>
+                            <input type="text" name="telephone" id="telephone"
+                                   placeholder="Telephone">
                         </div>
                         <div class="default-inp form-elem">
-                            <label for="company_logo">Company Logo</label>
-                            <input type="file" accept="image/*" name="company_logo" id="company_logo">
+                            <label for="mobile">Mobile</label>
+                            <input type="text" name="mobile" id="mobile"
+                                   placeholder="Mobile">
                         </div>
                         <div class="default-inp form-elem">
-                            <label for="email">Email</label>
-                            <input type="text" name="email" id="email"
-                                   placeholder="Email">
+                            <label for="fax">Fax</label>
+                            <input type="text" name="fax" id="fax"
+                                   placeholder="Fax">
                         </div>
                         <div class="default-inp form-elem">
-                            <label for="password">Password</label>
-                            <input type="password" name="password" id="password"
-                                   placeholder="********">
+                            <label for="skype">Skype</label>
+                            <input type="text" name="skype" id="skype"
+                                   placeholder="Skype">
                         </div>
                         <div class="default-inp form-elem">
-                            <label for="confirm_password">Confirm password</label>
-                            <input type="password" name="confirm_password" id="confirm_password"
-                                   placeholder="********">
+                            <label for="languages">Languages</label>
+                            <input type="text" name="languages" id="languages"
+                                   placeholder="Languages">
+                        </div>
+                        <div class="default-inp form-elem">
+                            <label for="company">Company</label>
+                            <input type="text" name="company" id="company"
+                                   placeholder="Company">
+                        </div>
+                        <div class="default-inp form-elem">
+                            <label for="website">Website</label>
+                            <input type="text" name="website" id="website"
+                                   placeholder="Website">
+                        </div>
+                        <div class="default-inp form-elem">
+                            <label for="about_me">About Me</label>
+                            <input type="textarea" name="about_me" id="about_me"
+                                   placeholder="About Me">
                         </div>
                         <div class="form-elem pull-right">
                             <button id="profile-btn" type="submit"
-                                    class="btn btn-primary next-step">Register</button>
+                                    class="btn btn-primary next-step">Save Profile</button>
                         </div>
                     </div>
-
                 </form>
             </div>
         </div>
