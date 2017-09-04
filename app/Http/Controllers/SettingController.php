0c00dba94413d87bace7cc41c0386e25e8afb1f2
xhulio <xhulian.hysollari@gmail.com>
2017-09-04 15:58:29 +0200

    activation fuckers

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -16,18 +16,14 @@ class SettingController extends Controller
         try {
             $value = Input::get('value');
             $title = Input::get('title');
-            $social = Setting::whereHas('translations', function ($query) use ($title) {
-                $query->where('locale', 'sq')
-                    ->where('title', $title);
-            })->where('type', 1)->first();
+            $social = Setting::where('title', $title)->where('type', 1)->first();
             if ($social) {
-                $social->translateOrNew('sq')->value = $value;
+                $social->value = $value;
             } else {
                 $social = new Setting();
                 $social->type = 1;
-                $social->save();
-                $social->translateOrNew('sq')->value = $value;
-                $social->translateOrNew('sq')->title = $title;
+                $social->value = $value;
+                $social->title = $title;
             }
             $social->save();
             return redirect()->back()->with('success', 'Success');
@@ -41,22 +37,18 @@ class SettingController extends Controller
         try {
             $value = Input::get('value');
             $title = Input::get('title');
-            $contact = Setting::whereHas('translations', function ($query) use ($title) {
-                $query->where('locale', 'sq')
-                    ->where('title', $title);
-            })->where('type', 0)->first();
+            $contact = Setting::where('title', $title)->where('type', 0)->first();
             if ($contact) {
-                $contact->translateOrNew('sq')->value = $value;
+                $contact->value = $value;
             } else {
                 $contact = new Setting();
                 $contact->type = 0;
-                $contact->save();
-                $contact->translateOrNew('sq')->value = $value;
-                $contact->translateOrNew('sq')->title = $title;
+                $contact->value = $value;
+                $contact->title = $title;
             }
             $contact->save();
             return redirect()->back()->with('success', 'Success');
-        } catch (\Exception $ex) {
+        }catch (\Exception $ex) {
             dd($ex->getMessage());
             return redirect()->back()->with('error', $ex->getMessage());
         }
@@ -65,28 +57,20 @@ class SettingController extends Controller
     public function about()
     {
         try {
-            $value_sq = Input::get('value_sq');
-            $value_en = Input::get('value_en');
+            $value = Input::get('value');
             $title = 'About Us';
-            $about = Setting::whereHas('translations', function ($query) use ($title) {
-                $query->where('locale', 'sq')
-                    ->where('title', $title);
-            })->where('type', 2)->first();
+            $about = Setting::where('title', $title)->where('type', 2)->first();
             if ($about) {
-                $about->translateOrNew('sq')->value = $value_sq;
-                $about->translateOrNew('en')->value = $value_en;
+                $about->value = $value;
             } else {
                 $about = new Setting();
                 $about->type = 2;
-                $about->save();
-                $about->translateOrNew('sq')->title = $title;
-                $about->translateOrNew('en')->title = $title;
-                $about->translateOrNew('sq')->value = $value_sq;
-                $about->translateOrNew('en')->value = $value_en;
+                $about->title = $title;
+                $about->value = $value;
             }
             $about->save();
             return redirect()->back()->with('success', 'Success');
-        } catch (\Exception $ex) {
+        }catch (\Exception $ex) {
             return redirect()->back()->with('error', $ex->getMessage());
         }
     }
@@ -94,24 +78,17 @@ class SettingController extends Controller
     public function mission()
     {
         try {
-            $value_sq = Input::get('value_sq');
-            $value_en = Input::get('value_en');
+            $value = Input::get('value');
             $title = 'mission';
-            $mission = Setting::whereHas('translations', function ($query) use ($title) {
-                $query->where('locale', 'sq')
-                    ->where('title', $title);
-            })->where('type', 3)->first();
+            $mission = Setting::where('title', $title)->where('type', 3)->first();
             if ($mission) {
-                $mission->translateOrNew('sq')->value = $value_sq;
-                $mission->translateOrNew('en')->value = $value_en;
+                $mission->value = $value;
             } else {
                 $mission = new Setting();
                 $mission->type = 3;
+                $mission->title = $title;
+                $mission->value = $value;
                 $mission->save();
-                $mission->translateOrNew('sq')->title = $title;
-                $mission->translateOrNew('en')->title = $title;
-                $mission->translateOrNew('sq')->value = $value_sq;
-                $mission->translateOrNew('en')->value = $value_en;
             }
             $mission->save();
             return redirect()->back()->with('success', 'Success');
@@ -123,31 +100,24 @@ class SettingController extends Controller
     public function payment()
     {
         try {
-            $value_sq = Input::get('value_sq');
-            $value_en = Input::get('value_en');
+            $value = Input::get('value');
             $title = Input::get('title');
             $optional = Input::get('optional');
-            $payment = Setting::whereHas('translations', function ($query) use ($title) {
-                $query->where('locale', 'sq')
-                    ->where('title', $title);
-            })->where('type', 5)->first();
+            $payment = Setting::where('title', $title)->where('type', 5)->first();
             if ($payment) {
-                $payment->translateOrNew('sq')->value = $value_sq;
-                $payment->translateOrNew('en')->value = $value_en;
+                $payment->value = $value;
+                $payment->value = $value;
                 $payment->optional = $optional;
             } else {
                 $payment = new Setting();
                 $payment->optional = $optional;
                 $payment->type = 5;
-                $payment->save();
-                $payment->translateOrNew('sq')->title = $title;
-                $payment->translateOrNew('en')->title = $title;
-                $payment->translateOrNew('sq')->value = $value_sq;
-                $payment->translateOrNew('en')->value = $value_en;
+                $payment->title = $title;
+                $payment->value = $value;
             }
             $payment->save();
             return redirect()->back()->with('success', 'Success');
-        } catch (\Exception $ex) {
+        }catch (\Exception $ex) {
             return redirect()->back()->with('error', $ex->getMessage());
         }
     }
@@ -155,28 +125,22 @@ class SettingController extends Controller
     public function ads()
     {
         try {
-            $value_sq = Input::get('value_sq');
+            $value = Input::get('value');
             $value_en = Input::get('value_en');
             $title = Input::get('title');
-            $ads = Setting::whereHas('translations', function ($query) use ($title) {
-                $query->where('locale', 'sq')
-                    ->where('title', $title);
-            })->where('type', 6)->first();
+            $ads = Setting::where('title', $title)->where('type', 6)->first();
             if ($ads) {
-                $ads->translateOrNew('sq')->value = $value_sq;
-                $ads->translateOrNew('en')->value = $value_en;
+                $ads->value = $value;
+                $ads->value = $value_en;
             } else {
                 $ads = new Setting();
                 $ads->type = 6;
-                $ads->save();
-                $ads->translateOrNew('sq')->title = $title;
-                $ads->translateOrNew('en')->title = $title;
-                $ads->translateOrNew('sq')->value = $value_sq;
-                $ads->translateOrNew('en')->value = $value_en;
+                $ads->title = $title;
+                $ads->value = $value;
             }
             $ads->save();
             return redirect()->back()->with('success', 'Success');
-        } catch (\Exception $ex) {
+        }catch (\Exception $ex) {
             return redirect()->back()->with('error', $ex->getMessage());
         }
     }
@@ -184,20 +148,15 @@ class SettingController extends Controller
     public function slider(SliderRequest $request)
     {
         try {
-            $value_sq = $request->get('value_sq');
-            $value_en = $request->get('value_en');
-            $title_sq = $request->get('title_sq');
-            $title_en = $request->get('title_en');
+            $value = $request->get('value');
+            $title = $request->get('title');
             $optional = $request->file('optional');
             $filename = $optional->store('settings');
             $slider = new Setting();
             $slider->optional = $filename;
             $slider->type = 4;
-            $slider->save();
-            $slider->translateOrNew('sq')->title = $title_sq;
-            $slider->translateOrNew('en')->title = $title_en;
-            $slider->translateOrNew('sq')->value = $value_sq;
-            $slider->translateOrNew('en')->value = $value_en;
+            $slider->title = $title;
+            $slider->value = $value;
             $slider->save();
             return redirect()->back()->with('success', 'Success');
         } catch (\Exception $ex) {
@@ -210,20 +169,15 @@ class SettingController extends Controller
     public function service(ServiceRequest $request)
     {
         try {
-            $value_sq = $request->get('value_sq');
-            $value_en = $request->get('value_en');
-            $title_sq = $request->get('title_sq');
-            $title_en = $request->get('title_en');
+            $value = $request->get('value');
+            $title = $request->get('title');
             $optional = $request->file('optional');
             $filename = $optional->store('settings');
             $service = new Setting();
             $service->optional = $filename;
             $service->type = 7;
-            $service->save();
-            $service->translateOrNew('sq')->title = $title_sq;
-            $service->translateOrNew('en')->title = $title_en;
-            $service->translateOrNew('sq')->value = $value_sq;
-            $service->translateOrNew('en')->value = $value_en;
+            $service->title = $title;
+            $service->value = $value;
             $service->save();
             return redirect()->back()->with('success', 'Success');
         } catch (\Exception $ex) {
