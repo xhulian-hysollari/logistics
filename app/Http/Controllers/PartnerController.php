9126a2a352f5e222f975a42d3887828af8d28521
xhulian-hysollari <xhulian.hysollari@cit.edu.al>
2017-09-06 14:45:09 +0200

    fixing partner stuff

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -86,7 +86,7 @@ class PartnerController extends Controller
     {
         try {
 
-            $partner_logo = $request->file('logo');
+            $partner_logo = $request->logo;
             $logo = $partner_logo->store('partners');
             $partner = Partner::where('id', $id)->first();
             $partner->name = $request->name;
