8a874c1c365e0892dd4bf239063a4e4a5f5cc7d3
xhulian-hysollari <xhulian.hysollari@cit.edu.al>
2017-09-06 15:59:38 +0200

    fixing partner stuff

--- resources/views/admin/partners/edit.blade.php
+++ resources/views/admin/partners/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.admin')
 
 @section('content')
-    {!! Form::model($result, ['route' => ['partner.update',  $result->id], 'method' => 'PATCH', 'class' => 'form-horizontal', 'novalidate']) !!}
+    {!! Form::model($result, ['route' => ['partner.update',  $result->id], 'method' => 'PATCH', 'class' => 'form-horizontal', 'novalidate', 'enctype' => 'multipart/form-data']) !!}
     @include('admin.partners.form')
     {!! Form::close() !!}
 
