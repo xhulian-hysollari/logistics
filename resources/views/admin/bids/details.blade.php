e0434d22671930bafd639df79aa951c94f990848
xhulio <xhulian.hysollari@gmail.com>
2017-09-11 15:54:17 +0200

    company logo shit

--- resources/views/admin/bids/details.blade.php
+++ resources/views/admin/bids/details.blade.php
@@ -1,93 +1,88 @@
 @extends('admin.admin')
 
 @section('content')
-
     <div class="row">
         <div class="col-md-12">
-
             <div class="panel panel-default">
-
-                <div class="col-xs-12 col-sm-6 col-md-6">
                 <div class="well well-sm">
-                    <div>
-                        <h4>
-                            {{$details->full_name}}</h4>
-                        <p>
-                            <i class="fa fa-envelope"></i> {{$details->email}}
-                            <br/>
-                            <i class="fa fa-phone"></i> {{$details->profile->telephone}}<br/>
-                            <i class="fa fa-skype"></i> {{$details->profile->skype}}</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-6 col-md-6">
-                <div class="well well-sm">
-                    <div>
+                    <div class="col-xs-12 col-sm-6 col-md-6">
                         <div>
                             <h4>
-                                {{$details->profile->name}}</h4>
+                                {{$details->full_name}}</h4>
                             <p>
-                                <i class="fa fa-envelope"></i> {{$details->profile->email}}
+                                <i class="fa fa-envelope"></i> {{$details->email}}
                                 <br/>
-                                <i class="fa fa-phone"></i> {{$details->profile->telephone}}
-                                <br/>
-                                <i class="fa fa-fax"></i> {{$details->profile->fax}}
-                                <br/>
-                                <i class="fa fa-globe"></i><a
-                                        href="{{$details->profile->website}}">{{$details->profile->website}}</a>
-                            </p>
+                                <i class="fa fa-phone"></i> {{$details->profile->telephone}}<br/>
+                                <i class="fa fa-skype"></i> {{$details->profile->skype}}</p>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-        <hr>
-        <div class="row main-grid">
-            <div class="col-sm-12">
-                <div class="well well-sm clearfix">
-                    <div>
-                        <div class="col-sm-12">
-                            <label>{{trans('freight.description')}}</label>
-                            <label>{{$bid->item->description}}</label>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="default-inp">
-                                <label>{{trans('truck.height')}}</label>
-                                <label>{{$bid->item->height}}</label>
-                            </div>
-                            <div class="default-inp">
-                                <label>{{trans('truck.length')}}</label>
-                                <label>{{$bid->item->length}}</label>
-                            </div>
-                            <div class="default-inp">
-                                <label>{{trans('truck.lorry_type')}}</label>
-                                <label>{{$bid->item->type}}</label>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="default-inp">
-                                <label>{{trans('truck.weight')}}</label>
-                                <<label>{{$bid->item->weight}}</label>
-                            </div>
-                            <div class="default-inp">
-                                <label>{{trans('truck.plate')}}</label>
-                                <label>{{$bid->item->plate}}</label>
-                            </div>
-                            <div class="default-inp">
-                                <label>{{trans('truck.location')}}</label>
-                                <label>{{$bid->item->location}}</label>
+                    <div class="col-xs-12 col-sm-6 col-md-6">
+                        <div>
+                            <div>
+                                <h4>
+                                    {{$details->profile->name}}</h4>
+                                <p>
+                                    <i class="fa fa-envelope"></i> {{$details->profile->email}}
+                                    <br/>
+                                    <i class="fa fa-phone"></i> {{$details->profile->telephone}}
+                                    <br/>
+                                    <i class="fa fa-fax"></i> {{$details->profile->fax}}
+                                    <br/>
+                                    <i class="fa fa-globe"></i><a
+                                            href="{{$details->profile->website}}">{{$details->profile->website}}</a>
+                                </p>
                             </div>
                         </div>
                     </div>
                 </div>
+                <hr>
+                <div class="row main-grid">
+                    <div class="col-sm-12">
+                        <div class="well well-sm clearfix">
+                            <div>
+                                <div class="col-sm-12">
+                                    <label>{{trans('freight.description')}}</label>
+                                    <label>{{$bid->item->description}}</label>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="default-inp">
+                                        <label>{{trans('truck.height')}}</label>
+                                        <label>{{$bid->item->height}}</label>
+                                    </div>
+                                    <div class="default-inp">
+                                        <label>{{trans('truck.length')}}</label>
+                                        <label>{{$bid->item->length}}</label>
+                                    </div>
+                                    <div class="default-inp">
+                                        <label>{{trans('truck.lorry_type')}}</label>
+                                        <label>{{$bid->item->type}}</label>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="default-inp">
+                                        <label>{{trans('truck.weight')}}</label>
+                                        <<label>{{$bid->item->weight}}</label>
+                                    </div>
+                                    <div class="default-inp">
+                                        <label>{{trans('truck.plate')}}</label>
+                                        <label>{{$bid->item->plate}}</label>
+                                    </div>
+                                    <div class="default-inp">
+                                        <label>{{trans('truck.location')}}</label>
+                                        <label>{{$bid->item->location}}</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                {{--<div class="row main-grid">--}}
+                {{--{!! Form::model($bid = new \App\Models\Bid(), ['route' => ['bid.freight', $bid->item->id, 0], 'method' => 'POST', 'class' => 'form-horizontal', 'novalidate']) !!}--}}
+                {{--@include('client.bid.form')--}}
+                {{--{!! Form::close() !!}--}}
+                {{--</div>--}}
             </div>
         </div>
-        <hr>
-        {{--<div class="row main-grid">--}}
-        {{--{!! Form::model($bid = new \App\Models\Bid(), ['route' => ['bid.freight', $bid->item->id, 0], 'method' => 'POST', 'class' => 'form-horizontal', 'novalidate']) !!}--}}
-        {{--@include('client.bid.form')--}}
-        {{--{!! Form::close() !!}--}}
-        {{--</div>--}}
-    </div>
     </div>
 @stop
