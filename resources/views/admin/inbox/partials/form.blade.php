a6b199a8f13d3983cd80255763936f25812c18e1
xhulio <xhulian.hysollari@gmail.com>
2017-09-12 16:36:58 +0200

    company logo shit

---
+++ resources/views/admin/inbox/partials/form.blade.php
@@ -0,0 +1,15 @@
+<div class="col-lg-12">
+    <form class="form" id="formCompose">
+        <div class="form-group floating-label">
+            <input type="email" class="form-control" id="to1" name="to1" >
+            <label for="to1">To</label>
+        </div>
+        <div class="form-group floating-label">
+            <input type="text" class="form-control" id="Subject1" name="Subject1" >
+            <label for="Subject1">Subject</label>
+        </div><!--end .form-group -->
+        <div class="form-group">
+            <textarea id="simple-summernote" name="message" class="form-control control-6-rows" spellcheck="false"></textarea>
+        </div><!--end .form-group -->
+    </form>
+</div><!--end .col -->
