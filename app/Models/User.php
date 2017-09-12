8546e63594d95a2354e4b3c92a223d9de79636d8
xhulio <xhulian.hysollari@gmail.com>
2017-09-12 17:11:49 +0200

    company logo shit

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,7 +42,7 @@ class User extends EloquentUser
 
     public function getUnreadConversationsAttribute(){
         return DB::table('message_statuses')
-            ->join('messages','message_state.message_id','=','messages.id')
+            ->join('messages','message_statuses.message_id','=','messages.id')
             ->join('conversation_user','message_statuses.conversation_id','=','conversation_user.conversation_id')
             ->where('messages.sent_by', '!=', $this->attributes['id'])
             ->where('conversation_user.user_id', $this->attributes['id'])
