4bdfca1acbb276e0505f5bfe6e80198952241e23
xhulio <xhulian.hysollari@gmail.com>
2017-09-12 17:03:59 +0200

    company logo shit

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Conversation extends Model
 {
@@ -17,5 +18,14 @@ class Conversation extends Model
 
     public function getLatestMessageAttribute(){
         return Message::where('conversation_id', $this->attributes['id'])->latest()->first();
+    }
+
+    public function getUnreadConversationsAttribute(){
+        return DB::table('message_state')
+            ->join('messages','message_state.message_id','=','messages.id')
+            ->where('messages.conversation_id', $this->attributes['id'])
+            ->select('messages.conversation_id', DB::raw('count(*) as count'))
+            ->groupBy('messages.conversation_id')
+            ->get();
     }
 }
