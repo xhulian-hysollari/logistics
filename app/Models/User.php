e3f10f1851fcf6bea645e9996346906a7181d020
xhulio <xhulian.hysollari@gmail.com>
2017-09-12 17:55:35 +0200

    company logo shit

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,7 +63,7 @@ class User extends EloquentUser
 //            $q->where('user_id', '=', $this->attributes['id'])->where('status', '=', 0);
 //
 //        })->groupBy('conversation_id')->select(['conversation_id'])->get();
-        return count(MessageState::where('user_id', $this->attributes['id'])->where('status',  0)->get());
+        return MessageState::where('user_id', $this->attributes['id'])->where('status',  0)->count();
     }
 
     public function getUnreadMessagesAttribute(){
