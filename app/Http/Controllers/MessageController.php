2f2a030f4a4aa5bf3c3d5fe785e72bb2be1f0d0d
xhulio <xhulian.hysollari@gmail.com>
2017-09-12 14:11:41 +0200

    company logo shit

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    public function index(){
+        $user = Sentinel::getUser();
+        return $user->conversations();
+    }
+}
