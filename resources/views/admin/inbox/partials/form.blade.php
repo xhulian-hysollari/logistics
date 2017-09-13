c5b7c3a00d002d736f4ff0d9c395b9097086a413
xhulio <xhulian.hysollari@gmail.com>
2017-09-13 17:44:32 +0200

    company logo shit

--- resources/views/admin/inbox/partials/form.blade.php
+++ resources/views/admin/inbox/partials/form.blade.php
@@ -1,6 +1,7 @@
 {{--<div class="col-lg-12">--}}
     {{--<form class="form" id="formCompose">--}}
         <div class="form-group">
+            <label for="simple-summernote">Enter your message below:</label>
             <textarea id="simple-summernote" name="message" class="form-control control-6-rows" spellcheck="false"></textarea>
         </div><!--end .form-group -->
     {{--</form>--}}
