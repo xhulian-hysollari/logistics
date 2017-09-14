9841cb4957f7c3beb1b2070d376f8b023677d219
xhulio <xhulian.hysollari@gmail.com>
2017-09-14 12:05:18 +0200

    company logo shit

--- resources/views/client/pages/page.blade.php
+++ resources/views/client/pages/page.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid block-content">
             <div class="row main-grid">
                 <div style="text-align: center; margin-bottom:40px;">
-                    <h2>{{$result->page_title}}</h2>
+                    <h2>{!! $result->page_title !!}</h2>
                 </div>
                 <div>
                     {!! $result->body !!}
