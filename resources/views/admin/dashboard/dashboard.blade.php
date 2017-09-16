e1813a3df1712e3da915e24f360f3afc6e2b9e41
xhulian-hysollari <xhulian.hysollari@cit.edu.al>
2017-09-16 13:08:30 +0200

    fixing partner stuff

--- resources/views/admin/dashboard/dashboard.blade.php
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -1,6 +1,62 @@
 @extends('admin.admin')
 @section('content')
     @if(Sentinel::inRole('admin'))
+
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <div id="embed-api-auth-container"></div>
+                    <div id="chart-container"></div>
+                    <div id="view-selector-container"></div>
+
+
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-head">
+                    <header>Registration history</header>
+                    <div class="tools">
+                        <a class="btn btn-icon-toggle btn-refresh"><i class="md md-refresh"></i></a>
+                        <a class="btn btn-icon-toggle btn-collapse"><i class="fa fa-angle-down"></i></a>
+                        <a class="btn btn-icon-toggle btn-close"><i class="md md-close"></i></a>
+                    </div>
+                </div><!--end .card-head -->
+                <div class="card-body no-padding height-9">
+                    <div class="row">
+                        <div class="col-sm-6 hidden-xs">
+                            <div class="force-padding text-sm text-default-light">
+                                <p>
+                                    <i class="md md-mode-comment text-primary-light"></i>
+                                    The registrations are measured from the time that the redesign was fully implemented
+                                    and after the first e-mailing.
+                                </p>
+                            </div>
+                        </div><!--end .col -->
+                        <div class="col-sm-6">
+                            <div class="force-padding pull-right text-default-light">
+                                <h2 class="no-margin text-primary-dark"><span class="text-xxl">66.05%</span></h2>
+                                <i class="fa fa-caret-up text-success fa-fw"></i> more registrations
+                            </div>
+                        </div><!--end .col -->
+                    </div><!--end .row -->
+                    <div class="stick-bottom-left-right force-padding">
+                        <div id="flot-registrations" class="flot height-5" data-title="Registration history"
+                             data-color="#0aa89e"></div>
+                    </div>
+                </div><!--end .card-body -->
+            </div><!--end .card -->
+        </div><!--end .col -->
+
+    @else
+
+
+
+    @endif
+@stop
+
 @section('js')
     <script>
         (function (w, d, s, g, js, fs) {
@@ -21,7 +77,7 @@
     </script>
     <script>
 
-        gapi.analytics.ready(function() {
+        gapi.analytics.ready(function () {
 
             /**
              * Authorize the user immediately if the user has already granted access.
@@ -71,18 +127,10 @@
             /**
              * Render the dataChart on the page whenever a new view is selected.
              */
-            viewSelector.on('change', function(ids) {
+            viewSelector.on('change', function (ids) {
                 dataChart.set({query: {ids: ids}}).execute();
             });
 
         });
     </script>
-@endsection
-
-@section('content')
-    <div id="embed-api-auth-container"></div>
-    <div id="chart-container"></div>
-    <div id="view-selector-container"></div>
-@endsection
-@endif
 @stop
