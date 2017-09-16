731b6ab76444e5e7002792ec52dfdd50313ff9f6
xhulian-hysollari <xhulian.hysollari@cit.edu.al>
2017-09-16 13:18:23 +0200

    fixing partner stuff

--- resources/views/admin/dashboard/dashboard.blade.php
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -140,6 +140,101 @@
     <script src="{{asset('admin/js/libs/flot/jquery.flot.orderBars.js')}}"></script>
     <script src="{{asset('admin/js/libs/flot/jquery.flot.pie.js')}}"></script>
     <script src="{{asset('admin/js/libs/flot/curvedLines.js')}}"></script>
-    <script src="{{asset('admin/js/core/demo/Demo.js')}}"></script>
-    <script src="{{asset('admin/js/core/demo/DemoDashboard.js')}}"></script>
+    <script>
+        var o = this;
+        var chart = $("#flot-registrations");
+
+        // Elements check
+        if (!$.isFunction($.fn.plot) || chart.length === 0) {
+            return;
+        }
+
+        // Chart data
+        var data = [
+            {
+                label: 'Registrations',
+                data: [
+                    [moment().subtract(11, 'month').valueOf(), 1100],
+                    [moment().subtract(10, 'month').valueOf(), 2450],
+                    [moment().subtract(9, 'month').valueOf(), 3800],
+                    [moment().subtract(8, 'month').valueOf(), 2650],
+                    [moment().subtract(7, 'month').valueOf(), 3905],
+                    [moment().subtract(6, 'month').valueOf(), 5250],
+                    [moment().subtract(5, 'month').valueOf(), 3600],
+                    [moment().subtract(4, 'month').valueOf(), 4900],
+                    [moment().subtract(3, 'month').valueOf(), 6200],
+                    [moment().subtract(2, 'month').valueOf(), 5195],
+                    [moment().subtract(1, 'month').valueOf(), 6500],
+                    [moment().valueOf(), 7805]
+                ],
+                last: true
+            }
+        ];
+
+        // Chart options
+        var labelColor = chart.css('color');
+        var options = {
+            colors: chart.data('color').split(','),
+            series: {
+                shadowSize: 0,
+                lines: {
+                    show: true,
+                    lineWidth: 2
+                },
+                points: {
+                    show: true,
+                    radius: 3,
+                    lineWidth: 2
+                }
+            },
+            legend: {
+                show: false
+            },
+            xaxis: {
+                mode: "time",
+                timeformat: "%b %y",
+                color: 'rgba(0, 0, 0, 0)',
+                font: {color: labelColor}
+            },
+            yaxis: {
+                font: {color: labelColor}
+            },
+            grid: {
+                borderWidth: 0,
+                color: labelColor,
+                hoverable: true
+            }
+        };
+        chart.width('100%');
+
+        // Create chart
+        var plot = $.plot(chart, data, options);
+
+        // Hover function
+        var tip, previousPoint = null;
+        chart.bind("plothover", function (event, pos, item) {
+            if (item) {
+                if (previousPoint !== item.dataIndex) {
+                    previousPoint = item.dataIndex;
+
+                    var x = item.datapoint[0];
+                    var y = item.datapoint[1];
+                    var tipLabel = '<strong>' + $(this).data('title') + '</strong>';
+                    var tipContent = y + " " + item.series.label.toLowerCase() + " on " + moment(x).format('dddd');
+
+                    if (tip !== undefined) {
+                        $(tip).popover('destroy');
+                    }
+                    tip = $('<div></div>').appendTo('body').css({left: item.pageX, top: item.pageY - 5, position: 'absolute'});
+                    tip.popover({html: true, title: tipLabel, content: tipContent, placement: 'top'}).popover('show');
+                }
+            }
+            else {
+                if (tip !== undefined) {
+                    $(tip).popover('destroy');
+                }
+                previousPoint = null;
+            }
+        });
+    </script>
 @stop
