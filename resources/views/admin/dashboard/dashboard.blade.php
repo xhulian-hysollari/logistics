7c883313b9fc32e6eabd9e399870c3d85c36aced
xhulian-hysollari <xhulian.hysollari@cit.edu.al>
2017-09-16 13:15:01 +0200

    fixing partner stuff

--- resources/views/admin/dashboard/dashboard.blade.php
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -140,4 +140,5 @@
     <script src="{{asset('js/libs/flot/jquery.flot.orderBars.js')}}"></script>
     <script src="{{asset('js/libs/flot/jquery.flot.pie.js')}}"></script>
     <script src="{{asset('js/libs/flot/curvedLines.js')}}"></script>
+    <script src="{{asset('js/core/demo/DemoDashboard.js')}}"></script>
 @stop
