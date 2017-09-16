2ee1a9d07fa1b4ecae2789175b65367b3038331c
xhulian-hysollari <xhulian.hysollari@cit.edu.al>
2017-09-16 13:33:17 +0200

    fixing partner stuff

--- resources/views/admin/dashboard/dashboard.blade.php
+++ resources/views/admin/dashboard/dashboard.blade.php
@@ -155,7 +155,7 @@
             {
                 label: 'Registrations',
                 data: [
-                    [moment().subtract(11, 'month').valueOf(), {{count(\App\Models\User::whereMonth('created_at', \Carbon\Carbon::now()->format('M')->subMonths(11))->whereYear('created_at', \Carbon\Carbon::now()->format('Y'))->get())}}],
+                    [moment().subtract(11, 'month').valueOf(), {{count(\App\Models\User::whereMonth('created_at', \Carbon\Carbon::now()->subMonths(11)->format('M'))->whereYear('created_at', \Carbon\Carbon::now()->format('Y'))->get())}}],
                     [moment().subtract(10, 'month').valueOf(), 2450],
                     [moment().subtract(9, 'month').valueOf(), 3800],
                     [moment().subtract(8, 'month').valueOf(), 2650],
