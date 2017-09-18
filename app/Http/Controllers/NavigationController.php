9bf79f0861a1c64ed2d7e002dc685608db2e61d0
xhulio <xhulian.hysollari@gmail.com>
2017-09-18 11:22:23 +0200

    company logo shit

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -71,7 +71,7 @@ class NavigationController extends Controller
                 }else{
                     $value = 0;
                 }
-                array_push($registrations, ['month' => $i, 'data' => $value]);
+                array_push($registrations[$i], $value);
         }
         foreach($registrations as $key => $registration){
             var_dump('key' . $key);
