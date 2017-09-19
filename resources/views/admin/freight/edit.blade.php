5ccdb90d7f57ffea2177117bb1f6a1de14ee7f92
xhulio <xhulian.hysollari@gmail.com>
2017-09-19 15:35:50 +0200

    company logo shit

--- resources/views/admin/freight/edit.blade.php
+++ resources/views/admin/freight/edit.blade.php
@@ -9,3 +9,19 @@
         </div>
     </div>
 @stop
+
+@section('js')
+    <script src="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyCiBlCK2sTv0-Dq_V098HVSJ7-lhLV13yU&libraries=places"></script>
+    <script>
+        function initDestination() {
+            var options = {
+            };
+
+            var loading = document.getElementById('loading');
+            var unloading = document.getElementById('unloading');
+            var loadcomplete = new google.maps.places.Autocomplete(loading, options);
+            var unloadcomplete = new google.maps.places.Autocomplete(unloading, options);
+        }
+        google.maps.event.addDomListener(window, 'load', initDestination);
+    </script>
+@stop
--- resources/views/admin/truck/edit.blade.php
+++ resources/views/admin/truck/edit.blade.php
@@ -5,3 +5,17 @@
         @include('admin.truck.form', [$button = trans('truck.update')])
     {!! Form::close() !!}
 @stop
+
+@section('js')
+    <script src="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyCiBlCK2sTv0-Dq_V098HVSJ7-lhLV13yU&libraries=places"></script>
+    <script>
+        function initDestination() {
+            var options = {
+            };
+
+            var location = document.getElementById('location');
+            var locationcomplete = new google.maps.places.Autocomplete(location, options);
+        }
+        google.maps.event.addDomListener(window, 'load', initDestination);
+    </script>
+@stop
