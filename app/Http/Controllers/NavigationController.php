437cafa7b3fe596dd99acca3d3ab593dcc41b3b8
xhulio <xhulian.hysollari@gmail.com>
2017-09-25 12:43:56 +0200

    company logo shit

--- app/Http/Controllers/NavigationController.php
+++ app/Http/Controllers/NavigationController.php
@@ -51,7 +51,32 @@ class NavigationController extends Controller
     {
 //        $user = Sentinel::getUser();
         $results = Contracts::all();
+
         return view('client.pages.tenders', compact('results'));
+    }
+
+    public function getTenders(){
+        $columns = array(
+            array( 'db' => 'first_name', 'dt' => 'first_name' ),
+            array( 'db' => 'last_name',  'dt' => 'last_name' ),
+            array( 'db' => 'position',   'dt' => 'position' ),
+            array( 'db' => 'office',     'dt' => 'office' ),
+            array(
+                'db'        => 'start_date',
+                'dt'        => 'start_date',
+                'formatter' => function( $d, $row ) {
+                    return date( 'jS M y', strtotime($d));
+                }
+            ),
+            array(
+                'db'        => 'salary',
+                'dt'        => 'salary',
+                'formatter' => function( $d, $row ) {
+                    return '$'.number_format($d);
+                }
+            )
+        );
+        return '';
     }
 
     public function getRegistrationPage()
