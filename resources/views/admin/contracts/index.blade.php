cb3f96907bbdfd6c59903b921149a9fc7cf2005e
xhulio <xhulian.hysollari@gmail.com>
2017-09-25 17:02:25 +0200

    company logo shit

--- resources/views/admin/contracts/index.blade.php
+++ resources/views/admin/contracts/index.blade.php
@@ -62,3 +62,32 @@
         </div>
     </div>
 @stop
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/components/bs-datatable.css')}}">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.2.4/css/buttons.dataTables.min.css">
+
+@stop
+@section('js')
+    <script type="text/javascript" src="{{asset('js/components/bs-datatable.js')}}"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.2.4/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.html5.min.js"></script>
+    <script>
+
+        $(document).ready(function() {
+            $('#data-table').dataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'copyHtml5',
+                    'excelHtml5',
+                    'csvHtml5',
+                    'pdfHtml5'
+                ]
+            });
+        });
+
+    </script>
+@stop
