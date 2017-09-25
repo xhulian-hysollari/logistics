4f511d99450263fcaeadb2131ba0b94efa5d6267
xhulio <xhulian.hysollari@gmail.com>
2017-09-25 17:51:46 +0200

    company logo shit

--- resources/views/client/pages/freight.blade.php
+++ resources/views/client/pages/freight.blade.php
@@ -1,9 +1,15 @@
 @extends('client.client')
 @section('content')
     <div class="block-content" style="padding-left: 20px; padding-right: 20px;">
+        @if(\Cartalyst\Sentinel\Laravel\Facades\Sentinel::check())
+            @if(\Cartalyst\Sentinel\Laravel\Facades\Sentinel::inRole('freight'))
+                <a href="{{route('freight.create')}}" class="btn btn-info"><i
+                            class="fa fa-plus"></i>{{trans('freight.add')}}</a>
+            @endif
+        @endif
         <div class="row main-grid">
             <div class="col-sm-12">
-                <table id="data-table" class="table table-responsive table-striped">
+                <table id="data_table" class="table table-responsive table-striped">
                     <thead>
                     <tr>
                         <th><i class="fa fa-map-marker"></i> Place of loading</th>
@@ -11,16 +17,19 @@
                         <th><i class="fa fa-cube"></i> {{trans('freight.volume')}}</th>
                         <th><i class="fa fa-truck"></i> {{trans('freight.lorry_type')}}</th>
                         <th><i class="fa fa-clock-o"></i> Listed at</th>
+                        <th>Actions</th>
                     </tr>
                     </thead>
                     <tbody>
                             @foreach($results as $freight)
-                                <tr class="table-row">
+                                <tr>
                                     <th>{{$freight->loading}}</th>
                                     <th>{{$freight->unloading}}</th>
                                     <th>{{$freight->volume}}</th>
                                     <th>{{$freight->type}}</th>
                                     <th>{{$freight->created_at}}</th>
+                                    <th><a href="{{route('freight.show', $freight->id)}}"><i class="fa fa-eye"></i></a>
+                                    </th>
                                 <tr>
                             @endforeach
                     </tbody>
@@ -45,7 +54,7 @@
     <script>
 
         $(document).ready(function() {
-            $('#data-table').dataTable({
+            $('#data_table').dataTable({
                 dom: 'Bfrtip',
                 buttons: [
                     'copyHtml5',
