b32371d5b54a6ca39f51b170e62b51c821f8be26
xhulio <xhulian.hysollari@gmail.com>
2017-09-25 17:32:53 +0200

    company logo shit

--- resources/views/client/pages/truck.blade.php
+++ resources/views/client/pages/truck.blade.php
@@ -9,14 +9,14 @@
                 @endif
             @endif
             <div class="col-sm-12">
-                <table class="table table-responsive table-striped">
+                <table id="data-table" class="table table-responsive table-striped">
                     <thead>
                     <tr>
                         <th>{{trans('truck.plate')}}</th>
                         <th>{{trans('truck.type')}}</th>
                         <th>{{trans('truck.location')}}</th>
                         <th>{{trans('truck.status')}}</th>
-                        <th></th>
+                        <th>Actions</th>
                     </tr>
                     </thead>
                     <tbody>
@@ -44,4 +44,43 @@
             </div>
         </div>
     </div>
+@stop
+
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/components/bs-datatable.css')}}">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.2.4/css/buttons.dataTables.min.css">
+
+@stop
+@section('js')
+    <script type="text/javascript" src="{{asset('js/components/bs-datatable.js')}}"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.2.4/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.html5.min.js"></script>
+    <script>
+
+        $(document).ready(function() {
+            $('#data-table thead th').each(function() {
+                var title = $('#data-table thead th').eq($(this).index()).text();
+                $(this).html('<input type="text" class="form-control" placeholder="Search ' + title + '" />');
+            });
+            var table = $('#data-table').DataTable();
+
+            table.columns().eq(0).each(function(colIdx) {
+                $('input', table.column(colIdx).header()).on('keyup change', function() {
+                    table
+                        .column(colIdx)
+                        .search(this.value)
+                        .draw();
+                });
+
+                $('input', table.column(colIdx).header()).on('click', function(e) {
+                    e.stopPropagation();
+                });
+            });
+        });
+
+    </script>
 @stop
