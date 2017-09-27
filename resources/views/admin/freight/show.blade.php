a2ac85a5eefb82c7669b6b82410b670df25df7a3
xhulio <xhulian.hysollari@gmail.com>
2017-09-27 09:16:28 +0200

    company logo shit

--- resources/views/admin/freight/show.blade.php
+++ resources/views/admin/freight/show.blade.php
@@ -225,7 +225,7 @@
                 directionsService.route({
                     origin: '{{$result->loading}}',
                     destination: '{{$result->unloading}}',
-                    travelMode: 'TRANSIT'
+                    travelMode: 'DRIVING'
                 }, function(response, status) {
                     if (status === 'OK') {
                         directionsDisplay.setDirections(response);
