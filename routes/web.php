2b4b44b0752fecff7b674341701ec9fa57e28dc5
xhulio <xhulian.hysollari@gmail.com>
2017-10-13 09:15:36 +0200

    company logo shit

--- routes/web.php
+++ routes/web.php
@@ -81,12 +81,15 @@ Route::middleware(['guest'])->group(function () {
             Route::get('/freights/show/{id}', ['as' => 'freight.show', 'uses' => 'FreightController@show']);
             Route::get('/freights/edit/{id}', ['as' => 'freight.edit', 'uses' => 'FreightController@edit']);
             Route::patch('/freights/update/{id}', ['as' => 'freight.update', 'uses' => 'FreightController@update']);
+            Route::post('/freights/delete/{id}', ['as' => 'freight.delete', 'uses' => 'FreightController@destroy']);
+
             Route::get('/trucks/index', ['as' => 'trucks.index', 'uses' => 'TruckController@index']);
             Route::get('/trucks/create', ['as' => 'trucks.create', 'uses' => 'TruckController@create']);
             Route::post('/trucks/store', ['as' => 'trucks.store', 'uses' => 'TruckController@store']);
             Route::get('/trucks/show/{id}', ['as' => 'trucks.show', 'uses' => 'TruckController@show']);
             Route::get('/trucks/edit/{id}', ['as' => 'trucks.edit', 'uses' => 'TruckController@edit']);
             Route::patch('/trucks/update/{id}', ['as' => 'trucks.update', 'uses' => 'TruckController@update']);
+            Route::post('/trucks/delete/{id}', ['as' => 'trucks.delete', 'uses' => 'TruckController@destroy']);
 
             /*
             |--------------------------------------------------------------------------
@@ -102,6 +105,7 @@ Route::middleware(['guest'])->group(function () {
             Route::get('/contracts/show/{id}', ['as' => 'contracts.show', 'uses' => 'ContractsController@show']);
             Route::get('/contracts/edit/{id}', ['as' => 'contracts.edit', 'uses' => 'ContractsController@edit']);
             Route::patch('/contracts/update/{id}', ['as' => 'contracts.update', 'uses' => 'ContractsController@update']);
+            Route::post('/contracts/delete/{id}', ['as' => 'contracts.delete', 'uses' => 'ContractsController@destroy']);
 
             Route::middleware(['hasExcalibur'])->group(function () {
                 /*
