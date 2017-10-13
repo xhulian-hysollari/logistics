fa957957868dbb691f166f728118ec6c4433a49a
xhulio <xhulian.hysollari@gmail.com>
2017-10-13 11:44:19 +0200

    company logo shit

--- resources/views/client/pages/truck.blade.php
+++ resources/views/client/pages/truck.blade.php
@@ -3,36 +3,38 @@
     <div class="block-content" style="padding-left: 20px; padding-right: 20px;">
         <div class="row main-grid">
             <div class="col-sm-12">
-                <input type="text" id="s-plate" placeholder="Search plate" />
+                <input type="text" id="s-plate" placeholder="Search plate"/>
+                <input type="text" id="s-type" placeholder="Search plate"/>
+                <input type="text" id="s-location" placeholder="Search plate"/>
                 <table id="data_table" class="table table-responsive table-striped">
                     <thead>
                     <tr>
                         <th>{{trans('truck.plate')}}</th>
                         <th>{{trans('truck.type')}}</th>
                         <th>{{trans('truck.location')}}</th>
-                        <th>{{trans('truck.status')}}</th>
+                        {{--<th>{{trans('truck.status')}}</th>--}}
                         <th style="display: none">Actions</th>
                     </tr>
                     </thead>
                     <tfoot>
                     {{--<tr>--}}
-                        {{--<th>{{trans('truck.plate')}}</th>--}}
-                        {{--<th>{{trans('truck.type')}}</th>--}}
-                        {{--<th>{{trans('truck.location')}}</th>--}}
-                        {{--<th>{{trans('truck.status')}}</th>--}}
-                        {{--<th style="display: none">Actions</th>--}}
+                    {{--<th>{{trans('truck.plate')}}</th>--}}
+                    {{--<th>{{trans('truck.type')}}</th>--}}
+                    {{--<th>{{trans('truck.location')}}</th>--}}
+                    {{--<th>{{trans('truck.status')}}</th>--}}
+                    {{--<th style="display: none">Actions</th>--}}
                     {{--</tr>--}}
                     </tfoot>
                     <tbody>
-                        @foreach($results as $truck)
-                            <tr>
-                                <td>{{$truck->plate}}</td>
-                                <td>{{$truck->type}}</td>
-                                <td>{{$truck->location}}</td>
-                                <td>{{$truck->status}}</td>
-                                <td><a href="{{route('trucks.show', $truck->id)}}"><i class="fa fa-eye fa-2x"></i></a></td>
-                            </tr>
-                        @endforeach
+                    @foreach($results as $truck)
+                        <tr>
+                            <td>{{$truck->plate}}</td>
+                            <td>{{$truck->type}}</td>
+                            <td>{{$truck->location}}</td>
+                            {{--<td>{{$truck->status}}</td>--}}
+                            <td><a href="{{route('trucks.show', $truck->id)}}"><i class="fa fa-eye fa-2x"></i></a></td>
+                        </tr>
+                    @endforeach
                     </tbody>
                 </table>
             </div>
@@ -70,18 +72,40 @@
 //            });
 
             var table = $('#data_table').DataTable();
-
+            var plate = $('#s-plate');
+            var type = $('#s-type');
+            var location = $('#s-location');
 //            table.columns().eq(0).each(function (colIdx) {
-                $('#s-plate').on('keyup change', function () {
-                    table
-                        .column(0)
-                        .search(this.value)
-                        .draw();
-                });
+            plate.on('keyup change', function () {
+                table
+                    .column(0)
+                    .search(this.value)
+                    .draw();
+            });
 
-                $('#s-plate').on('click', function (e) {
-                    e.stopPropagation();
-                });
+            plate.on('click', function (e) {
+                e.stopPropagation();
+            });
+            type.on('keyup change', function () {
+                table
+                    .column(1)
+                    .search(this.value)
+                    .draw();
+            });
+
+            $('#type').on('click', function (e) {
+                e.stopPropagation();
+            });
+            location.on('keyup change', function () {
+                table
+                    .column(0)
+                    .search(this.value)
+                    .draw();
+            });
+
+            location.on('click', function (e) {
+                e.stopPropagation();
+            });
 //            });
         });
 
