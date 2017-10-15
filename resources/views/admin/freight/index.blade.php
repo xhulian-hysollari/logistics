7d8fe960f4b26770476e1a4a85a8d75e1a551940
xhulio <xhulian.hysollari@gmail.com>
2017-10-15 10:51:49 +0200

    company logo shit

--- resources/views/admin/freight/index.blade.php
+++ resources/views/admin/freight/index.blade.php
@@ -3,65 +3,68 @@
 
     <div class="col-lg-12">
         <div class="card">
+            <div class="row">
             <a href="{{route('freight.create')}}" class="btn ink-reaction btn-primary pull-right" style="margin: 20px"> <i
                         class="fa fa-plus"></i>
                 Create
-                new</a>
+                new</a></div>
             <div class="card-body">
+                <div class="container-fluid" style="background-color: rgba(136,136,136,0.17); padding: 30px">
+                    <div class="row form-elem">
+                        <div class="col-sm-6 form-elem">
+                            <div class="default-inp form-elem">
+                                <input type="text" id="s-loading" placeholder="From"/>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 form-elem">
+                            <div class="default-inp form-elem">
+                                <input type="text" id="s-unloading" placeholder="To"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row form-elem">
+                        <div class="col-sm-6 form-elem">
+                            <div class="default-inp form-elem">
+                                <input type="text" id="s-volume" placeholder="Search volume"/>
+                            </div>
+                        </div>
+                        <div class="col-sm-6 form-elem">
+                            <div class="default-inp form-elem">
+                                <input type="text" id="s-type" placeholder="Search type"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row form-elem">
+                        <div class="col-sm-12 form-elem">
+                            <div class="default-inp form-elem">
+                                <input type="text" id="s-listed" placeholder="Search listing time"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="table-responsive">
                     <table class="table no-margin">
                         <thead>
                         <tr>
-                            <th>{{trans('freight.id')}}</th>
-                            @if(Sentinel::inRole('admin'))
-                                <th>Owner</th>
-                            @endif
-                            <th>{{trans('freight.weight')}}</th>
-                            <th>{{trans('freight.length')}}</th>
-                            <th>{{trans('freight.height')}}</th>
-                            <th>{{trans('freight.volume')}}</th>
-                            <th>Place of loading</th>
-                            <th>Place of unloading</th>
-                            <th>{{trans('freight.lorry_type')}}</th>
-                            <th>{{trans('freight.quantity')}}</th>
-                            <th>{{trans('freight.description')}}</th>
-                            <th>{{trans('freight.actions')}}</th>
+                            <th><i class="fa fa-map-marker"></i> Place of loading</th>
+                            <th><i class="fa fa-map-marker"></i> Place of unloading</th>
+                            <th><i class="fa fa-cube"></i> {{trans('freight.volume')}}</th>
+                            <th><i class="fa fa-truck"></i> {{trans('freight.lorry_type')}}</th>
+                            <th><i class="fa fa-clock-o"></i> Listed at</th>
+                            <th style="display: none">Actions</th>
                         </tr>
                         </thead>
                         <tbody>
-                        @if(isset($results))
-                            @foreach($results as $result)
-                                <tr>
-                                    <td>{{$result->freight_id}}</td>
-                                    @if(Sentinel::inRole('admin'))
-                                        <td>{{$result->owner->full_name }}</td>
-                                    @endif
-                                    <td>{{$result->weight}}</td>
-                                    <td>{{$result->length}}</td>
-                                    <td>{{$result->height}}</td>
-                                    <td>{{$result->volume}}</td>
-                                    <td>{{$result->loading}}</td>
-                                    <td>{{$result->unloading}}</td>
-                                    <td>{{$result->type}}</td>
-                                    <td>{{$result->quantity}}</td>
-                                    <td>{{$result->description}}</td>
-                                    <td>
-                                        @if(Sentinel::getUser()->id == $result->user_id)
-                                            <a href="{{ route('freight.edit', [$result->id]) }}"><i
-                                                        class="fa fa-pencil-square fa-2x"></i></a>
-                                            <a href="{{route('freight.delete', $result->id)}}"><i
-                                                        class="fa fa-trash-o fa-2x"></i></a>
-                                        @else
-                                            <a href="{{route('freight.show', $result->id)}}"><i class="fa fa-eye fa-2x"></i></a>
-                                        @endif
-                                        @if(Sentinel::inRole('admin') && Sentinel::getUser()->id != $result->user_id)
-                                            <a href="{{route('freight.delete', $result->id)}}"><i
-                                                        class="fa fa-trash-o fa-2x"></i></a>
-                                        @endif
-                                    </td>
-                                </tr>
-                            @endforeach
-                        @endif
+                        @foreach($results as $freight)
+                            <tr>
+                                <td>{{$freight->loading}}</td>
+                                <td>{{$freight->unloading}}</td>
+                                <td>{{$freight->volume}}</td>
+                                <td>{{$freight->type}}</td>
+                                <td>{{\Carbon\Carbon::parse($freight->created_at)->diffForHumans()}}</td>
+                                <td><a href="{{route('freight.show', $freight->id)}}"><i class="fa fa-eye fa-2x"></i></a></td>
+                            </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
