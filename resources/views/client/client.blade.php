c0ae191a89d4197db7bee4a6a05e57510681c378
xhulio <xhulian.hysollari@gmail.com>
2017-10-15 10:44:40 +0200

    company logo shit

--- resources/views/client/client.blade.php
+++ resources/views/client/client.blade.php
@@ -211,7 +211,7 @@
                  data-min450="1" data-min600="1" data-min768="1" style="margin-top: 50px;">
 
                 <div class="wow rotateIn" data-wow-delay="0.3s"><a href="#"><img style="width: 100%; height:118px  "
-                                                                                 src="{{asset('client/img/partner1.jpg')}}"
+                                                                                 src="{{asset('client/img/logo.png')}}"
                                                                                  alt="Img"></a></div>
 
 
@@ -220,7 +220,7 @@
                  data-min450="1" data-min600="1" data-min768="1" style="margin-top: 50px;">
 
                 <div class="wow rotateIn" data-wow-delay="0.3s"><a href="#"><img style="width: 100%; height:118px  "
-                                                                                 src="{{asset('client/img/partner1.jpg')}}"
+                                                                                 src="{{asset('client/img/logo.png')}}"
                                                                                  alt="Img"></a></div>
 
 
