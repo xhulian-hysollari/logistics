a48c01269add39e2b3cd4318d2e436338e1c608d
xhulio <xhulian.hysollari@gmail.com>
2017-10-15 10:59:52 +0200

    company logo shit

--- resources/views/admin/truck/index.blade.php
+++ resources/views/admin/truck/index.blade.php
@@ -33,7 +33,7 @@
                     </div>
                 </div></div>
                 <div class="table-responsive">
-                    <table class="table no-margin">
+                    <table class="table no-margin" id="data_table">
                         <thead>
                         <tr>
                             <th>{{trans('truck.plate')}}</th>
@@ -84,4 +84,53 @@
             </div>
         </div>
     </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="{{asset('css/components/bs-datatable.css')}}">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.2.4/css/buttons.dataTables.min.css">
+@stop
+@section('js')
+    <script type="text/javascript" src="{{asset('js/components/bs-datatable.js')}}"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.2.4/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/2.5.0/jszip.min.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.2.4/js/buttons.html5.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            var table = $('#data_table').DataTable();
+            var plate = $('#s-plate');
+            var type = $('#s-type');
+            var location = $('#s-location');
+            plate.on('keyup change', function () {
+                table
+                    .column(0)
+                    .search(this.value)
+                    .draw();
+            });
+            plate.on('click', function (e) {
+                e.stopPropagation();
+            });
+            type.on('keyup change', function () {
+                table
+                    .column(1)
+                    .search(this.value)
+                    .draw();
+            });
+            $('#type').on('click', function (e) {
+                e.stopPropagation();
+            });
+            location.on('keyup change', function () {
+                table
+                    .column(2)
+                    .search(this.value)
+                    .draw();
+            });
+            location.on('click', function (e) {
+                e.stopPropagation();
+            });
+        });
+
+    </script>
 @stop
