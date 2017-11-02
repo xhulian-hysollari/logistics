374be9323b9eb70bbc9c9daf40d7c292a7ef0c3b
xhulio <xhulian.hysollari@gmail.com>
2017-11-02 13:32:01 +0100

    company logo shit

--- app/Models/Truck.php
+++ app/Models/Truck.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -13,6 +14,10 @@ class Truck extends Model
 
     public function getOwnerAttribute(){
         return User::where('id', $this->attributes['user_id'])->first();
+    }
+
+    public function scopeActive($query){
+        return $query->whereDate('valid_until', '<', Carbon::parse($this->attributes['valid_until'])->addDays(3));
     }
 
 }
