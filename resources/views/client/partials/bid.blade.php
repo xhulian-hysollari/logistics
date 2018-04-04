debd4e3851b1edb8137ed019a026ec8e79e6158b
Xhulian <erkid.hoxholli@inovap.com>
2018-04-04 10:01:32 +0200

    about us

--- resources/views/client/partials/bid.blade.php
+++ resources/views/client/partials/bid.blade.php
@@ -1,10 +1,10 @@
 
-    <div class="default-inp form-elem">
+    <div class="bottommargin-sm">
         <label for="description">Write your bid</label>
         <textarea name="description" id="description" class="required sm-form-control input-block-level short-textarea"
                   placeholder="Write your bid here"></textarea>
     </div>
-    <div class="default-inp form-elem">
+    <div class="bottommargin-sm">
         <label for="files">Upload your attachments</label>
         <input type="file" class="required sm-form-control input-block-level" multiple name="files[]" id="files">
     </div>
