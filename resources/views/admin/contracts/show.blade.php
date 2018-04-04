7a756869e2b5aa267eba1ba0826d3a7c82214943
Xhulian <erkid.hoxholli@inovap.com>
2018-04-04 10:08:15 +0200

    about us

--- resources/views/admin/contracts/show.blade.php
+++ resources/views/admin/contracts/show.blade.php
@@ -1,156 +1,153 @@
-@extends('client.client')
+@extends('layout.index')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row main-grid">
-            <div class="well clearfix">
-                <div class="col-md-6">
+    <div class="container">
+        <div class="row">
+            <div class="col_half">
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Company: </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->company}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Email: </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->email}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Phone: </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->telephone}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Mobile: </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->mobile}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Fax: </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->fax}}
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="col_half">
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Representative: </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->full_name}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Skype: </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->skype}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Company Website: </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->website}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Languages Spoken: </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->languages}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Description: </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->about_me}}
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
     </div>
-    <div class="container-fluid">
-        <div class="row main-grid">
-            <div class="well clearfix">
-                <div class="col-md-6">
+    <div class="container">
+        <div class="row">
+                <div class="col_half">
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Contract Title </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->title}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Application Deadline </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{\Carbon\Carbon::parse($result->deadline)->format('d M Y')}}
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6">
+                <div class="col_half">
                     <div class="row">
-                        <div class="form-group">
-                            <div class="col-md-4">
+                        <div class="bottommargin-sm">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Contract Duration: </label>
                             </div>
-                            <div class="col-md-8">
+                            <div class="col_full nobottommargin">
                                 {{$result->duration}}
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-12">
+                <div class="col_full">
                     <div class="row">
-                        <div class="form-group">
+                        <div class="bottommargin-sm">
                             <div class="col-md-12">
                                 <label class="control-label">Description: </label>
                             </div>
@@ -160,7 +157,7 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="form-group">
+                        <div class="bottommargin-sm">
                             <div class="col-md-12">
                                 <label class="control-label">Requirements: </label>
                             </div>
@@ -171,22 +168,21 @@
                     </div>
                 </div>
             </div>
-        </div>
     </div>
     @if(Sentinel::check())
         @if(Sentinel::getUser()->id != $result->owner->id)
-            <div class="container-fluid block-content">
-                <div class="row main-grid">
-                    <form action="{{route('contract.bid', $result->id)}}" method="post" enctype="multipart/form-data">
+            <div class="container">
+                <div class="row">
+                    <form action="{{route('contract.bid', $result->id)}}" class="col_half" method="post" enctype="multipart/form-data">
                         @include('client.partials.bid')
                     </form>
                 </div>
             </div>
         @endif
     @else
-        <div class="container-fluid block-content">
-            <div class="row main-grid">
-                <form action="{{route('contract.bid', $result->id)}}" method="post" enctype="multipart/form-data">
+        <div class="container">
+            <div class="row">
+                <form action="{{route('contract.bid', $result->id)}}" class="col_half" method="post" enctype="multipart/form-data">
                     @include('client.partials.bid')
                 </form>
             </div>
