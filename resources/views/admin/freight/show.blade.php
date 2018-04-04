7f2ff1d7031dba3bc14b3470f0b63b8aeef4f4a1
Xhulian <erkid.hoxholli@inovap.com>
2018-04-04 09:55:05 +0200

    about us

--- resources/views/admin/freight/show.blade.php
+++ resources/views/admin/freight/show.blade.php
@@ -6,50 +6,50 @@
                 <div class="col_half">
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Company: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->company}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Email: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->email}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Phone: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->telephone}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Mobile: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->mobile}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Fax: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->fax}}
                             </div>
                         </div>
@@ -58,50 +58,50 @@
                 <div class="col_half col_last">
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Representative: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->full_name}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Skype: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->skype}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Company Website: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->website}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Languages Spoken: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->languages}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Description: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->owner->profile->about_me}}
                             </div>
                         </div>
@@ -114,30 +114,30 @@
                 <div class="col_half">
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Freight ID: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->freight_id}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Required Truck Type: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->type}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Dimensions: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 L: {{$result->length}}, H: {{$result->height}}, W: {{$result->weight}},
                                 V: {{$result->volume}}
                             </div>
@@ -145,40 +145,40 @@
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Loading: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->loading}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Unloading: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->unloading}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Quantity: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->quantity}}
                             </div>
                         </div>
                     </div>
                     <div class="row">
                         <div class="bottommargin-sm">
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 <label class="control-label">Description: </label>
                             </div>
-                            <div class="col_full">
+                            <div class="col_full nobottommargin">
                                 {{$result->description}}
                             </div>
                         </div>
