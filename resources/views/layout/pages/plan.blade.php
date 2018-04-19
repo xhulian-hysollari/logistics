c02356d1511cd481472d43a67cd3917e166d62ae
hx <xhulian.hysollari@inovap.com>
2018-04-19 11:23:38 +0200

    logo

--- resources/views/layout/pages/plan.blade.php
+++ resources/views/layout/pages/plan.blade.php
@@ -3,35 +3,48 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="fancy-title title-bottom-border"><h3>{{ $plan->name }}</h3></div>
-            <ul class="iconlist iconlist-color nobottommargin" style="list-style:none">
-                <li><i class="icon-ok"></i>Price: {{$plan->cost}}</li>
-                <li><i class="icon-ok"></i>Duration: {{$plan->duration}} ({{$plan->public_duration}})</li>
-                <li><i class="icon-ok"></i>{{$plan->freight_listing}} Freight Listings</li>
-                <li><i class="icon-ok"></i>{{$plan->truck_listing}} Truck Listings</li>
-                <li><i class="icon-ok"></i>{{$plan->tender_listing}} Tender Listings</li>
-                <li><i class="icon-ok"></i>{{$plan->ads}} Hour Ads</li>
-                <li>@if($plan->free_chat)<i class="icon-ok"></i> Free Chat @endif</li>
-                <li>@if($plan->route_planning)<i class="icon-ok"></i> Route Planning @endif</li>
-                <li>@if($plan->free_assistance)<i class="icon-ok"></i> Free Assistance from logistics agents @endif</li>
-                {{--<form action="{{ route('user.subscribe') }}" method="post">--}}
-                {{--{{ csrf_field() }}--}}
-                {{--<div id="dropin-container"></div>--}}
-                {{--<hr>--}}
-                {{--<input type="text" class="hidden" name="plan" value="{{ $plan->id }}">--}}
+            <div class="col_full col_last">
+                <div class="fancy-title title-bottom-border"><h3>{{ $plan->name }}</h3></div>
+            </div>
+            <div class="col_full col_last">
+                <ul class="iconlist iconlist-color nobottommargin" style="list-style:none">
+                    <li><i class="icon-ok"></i>Price: {{$plan->cost}}</li>
+                    <li><i class="icon-ok"></i>Duration: {{$plan->duration}} ({{$plan->public_duration}})</li>
+                    <li><i class="icon-ok"></i>{{$plan->freight_listing}} Freight Listings</li>
+                    <li><i class="icon-ok"></i>{{$plan->truck_listing}} Truck Listings</li>
+                    <li><i class="icon-ok"></i>{{$plan->tender_listing}} Tender Listings</li>
+                    <li><i class="icon-ok"></i>{{$plan->ads}} Hour Ads</li>
+                    <li>@if($plan->free_chat)<i class="icon-ok"></i> Free Chat @endif</li>
+                    <li>@if($plan->route_planning)<i class="icon-ok"></i> Route Planning @endif</li>
+                    <li>@if($plan->free_assistance)<i class="icon-ok"></i> Free Assistance from logistics agents @endif
+                    </li>
+                    {{--<form action="{{ route('user.subscribe') }}" method="post">--}}
+                    {{--{{ csrf_field() }}--}}
+                    {{--<div id="dropin-container"></div>--}}
+                    {{--<hr>--}}
+                    {{--<input type="text" class="hidden" name="plan" value="{{ $plan->id }}">--}}
 
-                {{--<button id="payment-button" class="btn btn-primary btn-flat hidden" type="submit">Pay--}}
-                {{--now--}}
-                {{--</button>--}}
-                {{--</form>--}}
-            </ul>
-            <p>{{$plan->description}}</p>
-            <label for="tos"></label>
-            <input type="checkbox" id="tos"> By continuing with the subscription I declare that I have read and
-            accepted the
-            <a href=""></a>
-            <a href="{{route('paypal.redirect', ['plan_id' => $plan->plan_id])}}"
-               class="button button-small button-3d nomargin">Paypal</a>
+                    {{--<button id="payment-button" class="btn btn-primary btn-flat hidden" type="submit">Pay--}}
+                    {{--now--}}
+                    {{--</button>--}}
+                    {{--</form>--}}
+                </ul>
+            </div>
+
+            <div class="col_full col_last">
+                <p>{{$plan->description}}</p>
+            </div>
+
+            <div class="col_full col_last">
+                <label for="tos"></label>
+                <input type="checkbox" id="tos"> By continuing with the subscription I declare that I have read and
+                accepted the
+            </div>
+
+            <div class="col_full col_last">
+                <a href="{{route('paypal.redirect', ['plan_id' => $plan->plan_id])}}"
+                   class="button button-small button-3d nomargin">Paypal</a>
+            </div>
         </div>
     </div>
 @stop
